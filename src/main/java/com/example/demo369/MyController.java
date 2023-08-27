509057755c4e7c42e06d732fd7ab94b3e5b7c115
raj77july <raj77july@gmail.com>
2023-08-26 23:08:16 -0700

    added change in controller content request mapping changed

--- src/main/java/com/example/demo369/MyController.java
+++ src/main/java/com/example/demo369/MyController.java
@@ -1,13 +1,15 @@
 package com.example.demo369;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/show")
 public class MyController {
 
     @GetMapping(value="/test")
   public String  test(){
-      return "Jai Shree Ram777";
+      return "Jai Shree Ram99";
   }
 }
