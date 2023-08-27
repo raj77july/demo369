1e75dd48a16536e13941246a30a97a1fc996c788
raj77july <raj77july@gmail.com>
2023-08-26 22:33:32 -0700

    added version first code

---
+++ src/test/java/com/example/demo369/Demo369ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo369;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo369ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
